05333f4c92a3c19d46fac2f1624e7535b254c88a
ok-val <ok.valnguyen@gmail.com>
2026-09-04 17:06:42 -0400

    Learn basic Responses API pattern

---
+++ src/TestApp.jsx
@@ -0,0 +1,12 @@
+import response2 from "../_orientation/openAI-responseAPI/migrate-to-responses";
+
+const TestApp = () => {
+  console.log(response2);
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  );
+};
+
+export default TestApp;
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router";
-import JayMort from "./JayMort/JayMort.jsx";
-import Chabot from "./Chabot/Chabot.jsx";
-import SAM from "./SAM/SAM.jsx";
-import Schon from "./SchonGPT/Schon.jsx"
+// import { BrowserRouter, Routes, Route, HashRouter } from "react-router";
+// import JayMort from "./JayMort/JayMort.jsx";
+// import Chabot from "./Chabot/Chabot.jsx";
+// import SAM from "./SAM/SAM.jsx";
+// import Schon from "./SchonGPT/Schon.jsx";
+import TestApp from "./TestApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    {/* <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/chabot" element={<Chabot />} />
@@ -18,6 +19,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/sam" element={<SAM />} />
         <Route path="/schon" element={<Schon />} />
       </Routes>
-    </HashRouter>
-  </StrictMode>
+    </HashRouter> */}
+    <TestApp />
+  </StrictMode>,
 );
